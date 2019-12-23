910ab6329593db783aa91c53d0a3dde6bcbde672
tomking2077 <tomking2077@gmail.com>
2019-12-23 09:48:31 -0600

    aws-flash-cards

--- sec3-2/src/temperatureApp/Calculator.js
+++ sec3-2/src/temperatureApp/Calculator.js
@@ -1,28 +1,48 @@
 import React, { Component } from "react";
 import BoilingVerdict from "./BoilingVerdict";
+import TemperatureInput from "./TemperatureInput";
 
 class Calculator extends Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        this.state = { temperature: '' };
+        this.state = {
+            temperature: 20,
+            scale: 'C'
+        };
+
     }
 
-    handleChange(e) {
-        this.setState({ temperature: e.target.value });
+    handleChange(e, _scale) {
+        this.setState({
+            temperature: e.target.value,
+            scale: _scale,
+
+        });
     }
 
     render() {
         const temperature = this.state.temperature;
+        const scale = this.state.scale;
+
+        let fTemp;
+        let cTemp;
+
+        if (scale == 'C') {
+            fTemp = Math.round((temperature * 9 / 5) + 32);
+            cTemp = temperature;
+        } else if (scale == 'F') {
+            cTemp = Math.round((temperature - 32) * 5 / 9);
+            fTemp = temperature;
+        }
+
         return (
             <fieldset>
-                <legend>Enter temperature in Celsius:</legend>
-                <input
-                    value={temperature}
-                    onChange={this.handleChange} />
+                <TemperatureInput temperature={fTemp} scale="F" handleChange={this.handleChange} />
+                <TemperatureInput temperature={cTemp} scale="C" handleChange={this.handleChange} />
 
                 <BoilingVerdict
-                    celsius={parseFloat(temperature)} />
+                    celsius={parseFloat(cTemp)} />
 
             </fieldset>
         );
