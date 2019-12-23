47d0ebfd33f1491f22d25f3c284fb1701f7fb59c
tomking <tomking@tamu.edu>
2019-12-23 08:15:42 -0600

    lift state

---
+++ sec3-2/src/FormPractice.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class FormPractice extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            name: ""
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("form submitted");
+        const name = document.getElementById("name").value;
+        console.log(name);
+    }
+
+    changeName = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col s6 offset-sm-3">
+
+                        <form onSubmit={this.handleSubmit}>
+                            <input onChange={this.changeName} value={this.state.name} type="text" id="name" placeholder="Enter name" />
+                            <input type="submit" value="submit" />
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FormPractice;
--- sec3-2/src/index.js
+++ sec3-2/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import FormPractice from './FormPractice';
 import * as serviceWorker from './serviceWorker';
+import Calculator from './temperatureApp/Calculator';
 
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<FormPractice />, document.getElementById('root'));
+//ReactDOM.render(<FormPractice />, document.getElementById('root'));
+ReactDOM.render(<Calculator />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
